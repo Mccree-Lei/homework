da229fa840b69d8c2591656c56c631c9a42281ed
Mccree-Lei <891305412@qq.com>
2019-08-07 19:19:53 +0800

    测试从eclipse提交推送项目

---
+++ testGitHub/src/com/lei/cn/Demo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.lei.cn;
+
+/**
+ * 【业务编码】：
+ * @工程： testGitHub
+ * @类名： Demo
+ * @模块： 
+ * @作者： 常雷雷
+ * @创建日期： 2019年8月7日
+ * @修改记录（修改时间、作者、原因）：
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
